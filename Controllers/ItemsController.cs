4b5de004ff61e4874c5875857838d7def9937eed
CaolanRice <c_f_rice@hotmail.co.uk>
2023-05-10 22:46:02 +0200

    gitignore

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -8,23 +8,29 @@ namespace Scith.Controllers
 {
     [ApiController]
     [Route("items")]
-    public class ItemsController : ControllerBase{
+    public class ItemsController : ControllerBase
+    {
         private readonly InMemItemsRepository repository;
 
-        public ItemsController(){
+        public ItemsController()
+        {
             repository = new InMemItemsRepository();
         }
 
         [HttpGet]
-        public IEnumerable<Item> GetItems(){
+        public IEnumerable<Item> GetItems()
+        {
             var items = repository.GetItems();
             return items;
         }
+
         [HttpGet("/{id}")]
         //actionresult allows for returning multiple types 
-        public ActionResult<Item> GetItem(Guid id){
+        public ActionResult<Item> GetItem(Guid id)
+        {
             var item = repository.GetItem(id);
-            if (item is null){
+            if (item is null)
+            {
                 return NotFound();
             }
             return item;
