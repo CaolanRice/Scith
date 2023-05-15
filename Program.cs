d439b26fb7d09f10d118669a2f9d3d1a4a411d4d
CaolanRice <c_f_rice@hotmail.co.uk>
2023-05-15 21:05:08 +0200

    set up mongodb connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 using Scith.Repositories;
+using dotenv.net;
+using MongoDB.Driver;
+using Scith.Configurations;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +15,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //only create one instance of repository over the lifetime of the service/application
-builder.Services.AddSingleton<InterfaceItemsRepository, ItemsRepository>();
+builder.Services.AddSingleton<InterfaceItemsRepository, MongoDbRepository>();
+//setup IMongoClient for dependency injection
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    //serialize guid and datetime to improve readability
+    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+    BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+    //retrive IConfiguration service instance to read configuration values from appsettings, .env etc
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    //retrieve configuration instance of our MongoDbConfig class
+    var mongoSettings = configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
+    //create new MongoClient instance using connection string from mongoSettings. 
+    //All requests of IMongoClient will use the same instance of this MongoClient instead of creating new ones
+    return new MongoClient(mongoSettings.ConnectionString);
+});
+
+
 
 var app = builder.Build();
 
@@ -27,3 +50,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+DotEnv.Load();
