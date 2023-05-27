6a41282b64d734f79accb2dbfb231949068fc32e
CaolanRice <c_f_rice@hotmail.co.uk>
2023-05-27 13:46:24 +0100

    add xunit framework

---
+++ Scith.UnitTests/ItemsControllerTests.cs
@@ -0,0 +1,16 @@
+namespace Scith.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    //Test naming convention. UnitOfWork_StateUnderTest_ExpectedBehaviour()
+    public void GetItemAsync_WithNullItem_ReturnsNotFound()
+    {
+        // Arrange
+        // var repositoryStub = new Mock
+
+        // Act
+
+        // Assert
+    }
+}
