2da00dc6b5c32b40a7082bff28135c0b991c2776
CaolanRice <c_f_rice@hotmail.co.uk>
2023-06-02 22:23:37 +0100

    get item return null test

--- Scith.UnitTests/ItemsControllerTests.cs
+++ Scith.UnitTests/ItemsControllerTests.cs
@@ -1,16 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Scith.API.Controllers;
+using Scith.API.Entities;
+
 namespace Scith.UnitTests;
 
-public class UnitTest1
+public class ItemsControllerTests
 {
+
+    private readonly Mock<InterfaceItemsRepository> repositoryStub = new();
+    private readonly Mock<ILogger<ItemsController>> loggerStub = new();
+    private readonly Random rand = new Random();
+
     [Fact]
     //Test naming convention. UnitOfWork_StateUnderTest_ExpectedBehaviour()
-    public void GetItemAsync_WithNullItem_ReturnsNotFound()
+    public async void GetItemAsync_WithNullItem_ReturnsNotFound()
     {
         // Arrange
-        // var repositoryStub = new Mock
+        repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((Item)null);
+
+        // var loggerStub = new Mock<ILogger<ItemsController>>();
+
+        var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
 
         // Act
+        var result = await controller.GetItemAsync(Guid.NewGuid());
 
         // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
+
     }
+
+    [Fact]
+    //Test naming convention. UnitOfWork_StateUnderTest_ExpectedBehaviour()
+    public async void GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+    {
+
+
+    }
+
+    // private Item CreateItem()
+    // {
+    //     return new()
+    //     {
+    //         Id = Guid.NewGuid(),
+    //         Name = Guid.NewGuid().ToString();
+            
+    //     }
+    // }
 }
